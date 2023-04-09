f651a26f00de43e8f8e12548c7c233e3f42b3d04
Pedro Foresti Leão <forestileao@pop-os.localdomain>
2023-04-09 03:37:11 -0300

    first commit

---
+++ lib/super_sizer.ex
@@ -0,0 +1,18 @@
+defmodule SuperSizer do
+  @moduledoc """
+  Documentation for `SuperSizer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SuperSizer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/super_sizer_test.exs
@@ -0,0 +1,8 @@
+defmodule SuperSizerTest do
+  use ExUnit.Case
+  doctest SuperSizer
+
+  test "greets the world" do
+    assert SuperSizer.hello() == :world
+  end
+end
